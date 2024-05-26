59c4c3308dba59fdd736190f2cdae9a5010976e2
TBot <TrongThinh2602@gmail.com>
2024-05-26 20:01:20 +0700

    View - Guest Home Page

--- BarBob/Program.cs
+++ BarBob/Program.cs
@@ -70,7 +70,9 @@ app.MapControllerRoute(
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{action=Index}/{id?}",
+    defaults: new { controller = "Home" });
+
 app.MapRazorPages();
 
 app.Run();
