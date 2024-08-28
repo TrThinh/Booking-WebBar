08edecf412bcc1ab4df8fbe82bfde8ee2b27d79c
TBot <TrongThinh2602@gmail.com>
2024-08-28 21:25:33 +0700

    Manager - CRUD User

---
+++ BarBob/Areas/Manager/Controllers/ManageUserController.cs
@@ -0,0 +1,172 @@
+﻿using BarBob.Models;
+using BarBob.Models.ViewModels;
+using BarBob.Repository;
+using BarBob.Repository.IRepository;
+using BarBob.Utility;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+
+namespace BarBob.Areas.Manager.Controllers
+{
+    [Area("Manager")]
+    [Authorize(Roles = SD.Role_Manager)]
+    public class ManageUserController : Controller
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public ManageUserController(UserManager<IdentityUser> userManager, RoleManager<IdentityRole> roleManager, IUnitOfWork unitOfWork)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+            _unitOfWork = unitOfWork;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            UserVM userVM = CreateUserVM();
+
+            return View(userVM);
+        }
+
+        private User CreateUser()
+        {
+            try
+            {
+                return Activator.CreateInstance<User>();
+            }
+            catch
+            {
+                throw new InvalidOperationException($"Can't create an instance of '{nameof(User)}'. " +
+                    $"Ensure that '{nameof(User)}' is not an abstract class and has a parameterless constructor, or alternatively " +
+                    $"override the register page in /Areas/Identity/Pages/Account/Register.cshtml");
+            }
+        }
+
+        private UserVM CreateUserVM()
+        {
+            UserVM userVM = new UserVM()
+            {
+                User = new User(),
+                RoleList = _roleManager.Roles
+                    .Where(role => role.Name != SD.Role_Manager && role.Name != "Admin")
+                    .Select(i => new SelectListItem
+                    {
+                        Text = i.Name,
+                        Value = i.Name
+                    })
+            };
+            return userVM;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(UserVM userVM)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = CreateUser();
+
+                user.FirstName = userVM.User.FirstName;
+                user.LastName = userVM.User.LastName;
+                user.Email = userVM.User.Email;
+                user.UserName = userVM.User.Email;
+                user.PhoneNumber = userVM.User.PhoneNumber;
+                user.Birthday = userVM.User.Birthday;
+                user.EmailConfirmed = true;
+
+                var result = await _userManager.CreateAsync(user, userVM.Password);
+
+                if (result.Succeeded)
+                {
+                    if (!String.IsNullOrEmpty(userVM.User.Role))
+                    {
+                        await _userManager.AddToRoleAsync(user, userVM.User.Role);
+                    }
+                    else
+                    {
+                        await _userManager.AddToRoleAsync(user, SD.Role_Customer);
+                    }
+                    TempData["Success"] = "User created successfully";
+                    return Redirect("Index");
+                }
+                else
+                {
+                    foreach (var error in result.Errors)
+                    {
+                        ModelState.AddModelError(string.Empty, error.Description);
+                    }
+                }
+            }
+            UserVM newUserVM = CreateUserVM();
+            newUserVM.User = userVM.User;
+            TempData["Error"] = "Error creating user";
+            return View(newUserVM);
+        }
+
+
+        #region API CALLS
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var users = _unitOfWork.User.GetAllIncluding().ToList();
+            var userList = new List<User>();
+
+            foreach (var user in users)
+            {
+                var roles = await _userManager.GetRolesAsync(user);
+                user.Role = roles.FirstOrDefault();
+                userList.Add(user);
+            }
+
+            return Json(new { data = userList });
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> DeleteUser(string id)
+        {
+            if (string.IsNullOrEmpty(id))
+            {
+                return BadRequest("User ID is required");
+            }
+
+            var user = await _userManager.FindByIdAsync(id);
+
+            if (user == null)
+            {
+                return NotFound("User not found");
+            }
+
+            // If user has role is Admin will display "message"
+            var roles = await _userManager.GetRolesAsync(user);
+            if (roles.Contains("Admin"))
+            {
+                return BadRequest(new { success = false, message = "Cannot delete a user with the Admin role" });
+            }
+
+            var deleteResult = await _userManager.DeleteAsync(user);
+
+            if (deleteResult.Succeeded)
+            {
+                _unitOfWork.Save();
+                return Ok(new { success = true, message = "User deleted successfully" });
+            }
+            else
+            {
+                return BadRequest(new { success = false, message = "Error while deleting user" });
+            }
+        }
+
+
+        #endregion
+    }
+}
---
+++ BarBob/wwwroot/js/user-manager.js
@@ -0,0 +1,113 @@
+﻿var dataTable;
+
+$(document).ready(function () {
+    loadDataTable();
+    closeForm();
+});
+
+function loadDataTable() {
+    dataTable = $('#user-table').DataTable({
+        "ajax": { url: '/manager/manageuser/getall' },
+        "columns": [
+            { "data": "firstName", "width": "15%", "className": "table-cell" },
+            { "data": "lastName", "width": "15%", "className": "table-cell" },
+            { "data": "email", "width": "10%", "className": "table-cell" },
+            { "data": "phoneNumber", "width": "12%", "className": "table-cell" },
+            { "data": "birthday", "width": "12%", "className": "table-cell" },
+            { "data": "role", "width": "7%", "className": "table-cell" },
+            {
+                data: { id: "id" },
+                "render": function (data) {
+                    return `
+                        <div class="text-center">
+                            <button onclick=deleteUser('${data.id}') class="btn btn-danger text-white" style="cursor:pointer; width:80px;">
+                                <i class="bi bi-trash-fill"></i> Delete
+                            </button>
+                        </div>
+                    `;
+                },
+                "width": "10%",
+                "className": "table-cell"
+            }
+        ]
+    });
+
+    //CSS to shorten data when it's too long
+    var css = '.table-cell { max-width: 180px; overflow: hidden; text-overflow: ellipsis; white-space: nowrap; }',
+        head = document.head || document.getElementsByTagName('head')[0],
+        style = document.createElement('style');
+
+    head.appendChild(style);
+
+    style.type = 'text/css';
+    if (style.styleSheet) {
+        // This is required for IE8 and below.
+        style.styleSheet.cssText = css;
+    } else {
+        style.appendChild(document.createTextNode(css));
+    }
+}
+
+function closeForm() {
+    $('#close-form').click(function () {
+        Swal.fire({
+            title: 'Are you sure you want to close?',
+            text: 'You will be redirected to the Manage User page.',
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, close'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                window.location.href = "/Manager/ManageUser";
+            }
+        });
+    });
+}
+
+function deleteUser(userId) {
+    Swal.fire({
+        title: 'Are you sure?',
+        text: "You won't be able to revert this!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Yes, delete it!'
+    }).then((result) => {
+        if (result.isConfirmed) {
+            fetch(`/manager/manageuser/deleteUser/${userId}`, {
+                method: 'DELETE',
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            })
+                .then(response => response.json())
+                .then(data => {
+                    if (data.success) {
+                        Swal.fire(
+                            'Deleted!',
+                            data.message,
+                            'success'
+                        );
+                        dataTable.ajax.reload();
+                    } else {
+                        Swal.fire(
+                            'Error!',
+                            data.message,
+                            'error'
+                        );
+                    }
+                })
+                .catch(error => {
+                    console.error("Error:", error);
+                    Swal.fire(
+                        'Error!',
+                        "An error occurred during deletion",
+                        'error'
+                    );
+                });
+        }
+    });
+}
