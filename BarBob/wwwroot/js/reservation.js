c5010854e5072ae187127bc9d6f8d1349965f32a
TBot <TrongThinh2602@gmail.com>
2024-09-08 19:49:33 +0700

    Customer - View Reservation & Reponsive

--- BarBob/wwwroot/js/reservation.js
+++ BarBob/wwwroot/js/reservation.js
@@ -1,8 +1,8 @@
 ﻿document.getElementById("reservationForm").addEventListener("submit", function (event) {
-    event.preventDefault(); // Ngăn việc submit form và refresh trang
+    event.preventDefault();
 
     var formData = new FormData(this);
-    formData.append("TableList", JSON.stringify(tableList)); // Gửi dữ liệu TableList
+    formData.append("TableList", JSON.stringify(tableList));
 
     fetch('/Customer/Reservation/BookTable', {
         method: 'POST',
@@ -18,20 +18,19 @@
             if (data.success) {
                 resultMessage.className = "alert alert-success";
                 resultMessage.innerText = data.message;
-                console.log("Success:", data.message); // Log thông báo thành công ra console
+                console.log("Success:", data.message);
             } else {
                 resultMessage.className = "alert alert-danger";
                 resultMessage.innerText = data.message;
 
-                // Hiển thị chi tiết lỗi nếu có
                 if (data.errors && data.errors.length > 0) {
-                    console.log("Error details:", data.errors); // Log lỗi chi tiết ra console
+                    console.log("Error details:", data.errors);
                     data.errors.forEach(error => {
-                        console.log("Error:", error); // Hiển thị từng lỗi
+                        console.log("Error:", error);
                     });
                 }
 
-                console.log("Error:", data.message); // Log thông báo lỗi ra console
+                console.log("Error:", data.message);
             }
 
             resultMessage.style.display = "block";
